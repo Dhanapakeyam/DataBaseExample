c7f8242119b5dbe62d6708a22e86a9411567415f
Dhanapakeyam <m.dhana87@gmail.com>
2022-12-30 18:16:09 -0800

    modified console log

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ dotenv.config();
 const app = express();
 
 const PORT = 4000;
-
+console.log(process.env.MONGO_URL);
 //connection code
 //const MONGO_URL = "mongodb://127.0.0.1";
 const MONGO_URL = process.env.MONGO_URL;
